7dd002d2726a2e26fb32fbd8c9402faca04e9fe8
eric <psytraxx@gmail.com>
2024-12-19 21:54:42 +0100

    works

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@
 
 use core::cell::RefCell;
 
-use defmt::info;
+use defmt::{error, info};
 use display_interface_spi::SPIInterface;
 use embassy_embedded_hal::shared_bus::blocking::spi::SpiDevice;
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::NoopMutex;
 use embedded_graphics_core::prelude::RgbColor;
 use esp_alloc::psram_allocator;
-use esp_hal::gpio::{Level, Output};
+use esp_hal::gpio::{Input, Level, Output};
+use esp_hal::i2c::master::I2c;
 use esp_hal::rng::Rng;
 use esp_hal::spi::master::Config;
 use esp_hal::spi::SpiMode;
@@ -25,7 +26,6 @@ use static_cell::StaticCell;
 use {defmt_rtt as _, esp_backtrace as _};
 extern crate alloc;
 
-mod display;
 mod rm67162;
 
 //https://github.com/Xinyuan-LilyGO/LilyGo-AMOLED-Series/blob/8c72b786373fbaef46ce35a6db924d6e16a0c3ec/src/LilyGo_AMOLED.cpp#L806
@@ -50,18 +50,114 @@ async fn main(_spawner: Spawner) -> ! {
 
     psram_allocator!(peripherals.PSRAM, esp_hal::psram);
 
+    let mut i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
+        .with_sda(peripherals.GPIO3)
+        .with_scl(peripherals.GPIO2);
+
+    // Try to find 1.91 inch i2c devices
+    if i2c.write(0x15, &[]).is_ok() {
+        // Check RTC Slave address
+        if i2c.write(0x51, &[]).is_ok() {
+            info!("Detect 1.91-inch SPI board model!");
+        } else {
+            info!("Detect 1.91-inch QSPI board model!");
+        }
+    } else {
+        error!("Unable to detect 1.91-inch touch board model!");
+    }
+
+    /*static const  BoardsConfigure_t BOARD_AMOLED_191_SPI = {
+        // RM67162 Driver
+        RM67162_AMOLED_SPI,
+        &AMOLED_191_TOUCH_PINS,     //Touch CST816T
+        &AMOLED_191_SPI_PMU_PINS,   //PMU
+        NULL,                       //SENSOR
+        &AMOLED_191_SPI_SD_PINS,    //SDCard
+        AMOLED_191_BUTTONTS,        //Button Pins
+        1, //Button Number
+        -1,//pixelsPins
+        4, //adcPins
+        38,//PMICEnPins
+        false,//framebuffer
+    }; */
+
+    /*// LILYGO 1.91 Inch AMOLED(RM67162) S3R8
+    // https://www.lilygo.cc/products/t-display-s3-amoled
+    static const DisplayConfigure_t RM67162_AMOLED_SPI  = {
+        18,//BOARD_DISP_DATA0,          //MOSI
+        7,//BOARD_DISP_DATA1,           //DC
+        -1,//BOARD_DISP_DATA2,
+        -1,//BOARD_DISP_DATA3,
+        47,//BOARD_DISP_SCK,            //SCK
+        6,//BOARD_DISP_CS,              //CS
+        BOARD_NONE_PIN,//DC
+        17,//BOARD_DISP_RESET,          //RST
+        9, //BOARD_DISP_TE,
+        8, //command bit
+        24,//address bit
+        40000000,
+        (lcd_cmd_t *)rm67162_spi_cmd,
+        RM67162_INIT_SPI_SEQUENCE_LENGTH,
+        RM67162_WIDTH,//width
+        RM67162_HEIGHT,//height
+        0,//frameBufferSize
+        false //fullRefresh
+    }; */
+
+    /*typedef struct __DisplayConfigure {
+        int d0;
+        int d1;
+        int d2;
+        int d3;
+        int sck;
+        int cs;
+        int dc;
+        int rst;
+        int te;
+        uint8_t cmdBit;
+        uint8_t addBit;
+        int  freq;
+        lcd_cmd_t *initSequence;
+        uint32_t initSize;
+        uint16_t width;
+        uint16_t height;
+        uint32_t frameBufferSize;
+        bool fullRefresh;
+    } DisplayConfigure_t; */
+
+    /*static const  BoardsConfigure_t BOARD_AMOLED_191_SPI = {
+        // RM67162 Driver
+        RM67162_AMOLED_SPI,
+        &AMOLED_191_TOUCH_PINS,     //Touch CST816T
+        &AMOLED_191_SPI_PMU_PINS,   //PMU
+        NULL,                       //SENSOR
+        &AMOLED_191_SPI_SD_PINS,    //SDCard
+        AMOLED_191_BUTTONTS,        //Button Pins
+        1, //Button Number
+        -1,//pixelsPins
+        4, //adcPins
+        38,//PMICEnPins
+        false,//framebuffer
+    }; */
+
     let delay = Delay::new();
 
     // SPI pins
     let sck = Output::new(peripherals.GPIO47, Level::Low);
     let mosi = Output::new(peripherals.GPIO18, Level::Low);
-    let cs = Output::new(peripherals.GPIO6, Level::Low);
+    let cs = Output::new(peripherals.GPIO6, Level::High);
+
+    let _te = Input::new(peripherals.GPIO9, esp_hal::gpio::Pull::Down);
+
+    let mut pmicen = Output::new_typed(peripherals.GPIO38, Level::Low);
+    pmicen.set_high();
+    info!("PMICEN set high");
 
     // Configure SPI
     let spi = Spi::new_with_config(
         peripherals.SPI2,
         Config {
-            frequency: 40.MHz(),
+            frequency: 40.MHz(), //TODO: 40MHz
             mode: SpiMode::Mode0,
             ..Config::default()
         },
@@ -74,30 +170,20 @@ async fn main(_spawner: Spawner) -> ! {
     let dc_pin = peripherals.GPIO7;
     let rst_pin = peripherals.GPIO17;
 
-    let spi: SpiDevice<
-        '_,
-        embassy_sync::blocking_mutex::raw::NoopRawMutex,
-        Spi<'_, Blocking>,
-        Output<'_>,
-    > = SpiDevice::new(spi_bus, cs);
+    let spi = SpiDevice::new(spi_bus, cs);
 
     // Initialize the display
-
-    //let mut driver = DisplayDriver::new(spi, delay);
-    //driver.init(dc_pin, rst_pin);
-
     let di = SPIInterface::new(spi, Output::new(dc_pin, Level::Low));
 
     let mut display = Builder::new(RM67162, di)
         .orientation(Orientation::default())
         .display_size(240, 536)
-        .reset_pin(Output::new(rst_pin, Level::Low))
+        .reset_pin(Output::new(rst_pin, Level::High))
         .init(&mut embassy_time::Delay)
         .unwrap();
 
     loop {
-        info!("Hello world!");
-        delay.delay(500.millis());
+        delay.delay(100.micros());
         let x = rng.random() % 240;
         let y = rng.random() % 536;
         display
--- src/rm67162.rs
+++ src/rm67162.rs
@@ -1,13 +1,136 @@
+use defmt::info;
 use display_interface::{DataFormat, WriteOnlyDataCommand};
-use embedded_graphics_core::pixelcolor::Rgb565;
+use embedded_graphics_core::{pixelcolor::Rgb565, prelude::IntoStorage};
 use embedded_hal::delay::DelayNs;
 use embedded_hal::digital::OutputPin;
 use mipidsi::{
-    dcs::{BitsPerPixel, Dcs, PixelFormat, SetAddressMode},
+    dcs::{Dcs, SetAddressMode, SoftReset, WriteMemoryStart},
     error::{Error, InitError},
     models::Model,
     options::ModelOptions,
 };
+
+// Define a structure for the LCD command
+struct LcdCommand<'a> {
+    addr: u8,                 // Command address
+    params: &'a [u8],         // Command parameters
+    delay_after: Option<u32>, // Delay in milliseconds after sending the command
+}
+
+// AMOLED initialization commands
+const AMOLED_INIT_CMDS: &[LcdCommand] = &[
+    /*const lcd_cmd_t rm67162_spi_cmd[RM67162_INIT_SPI_SEQUENCE_LENGTH] = {
+        {0xFE, {0x04}, 0x01}, //SET APGE3
+        {0x6A, {0x00}, 0x01},
+        {0xFE, {0x05}, 0x01}, //SET APGE4
+        {0xFE, {0x07}, 0x01}, //SET APGE6
+        {0x07, {0x4F}, 0x01},
+        {0xFE, {0x01}, 0x01}, //SET APGE0
+        {0x2A, {0x02}, 0x01},
+        {0x2B, {0x73}, 0x01},
+        {0xFE, {0x0A}, 0x01}, //SET APGE9
+        {0x29, {0x10}, 0x01},
+        {0xFE, {0x00}, 0x01},
+        {0x51, {AMOLED_DEFAULT_BRIGHTNESS}, 0x01},
+        {0x53, {0x20}, 0x01},
+        {0x35, {0x00}, 0x01},
+
+        {0x3A, {0x75}, 0x01}, // Interface Pixel Format 16bit/pixel
+        {0xC4, {0x80}, 0x01},
+        {0x11, {0x00}, 0x01 | 0x80},
+        {0x29, {0x00}, 0x01 | 0x80},
+    }; */
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x04],
+        delay_after: None,
+    }, // SET APGE3
+    LcdCommand {
+        addr: 0x6A,
+        params: &[0x00],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x05],
+        delay_after: None,
+    }, // SET APGE4
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x07],
+        delay_after: None,
+    }, // SET APGE6
+    LcdCommand {
+        addr: 0x07,
+        params: &[0x4F],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x01],
+        delay_after: None,
+    }, // SET APGE0
+    LcdCommand {
+        addr: 0x2A,
+        params: &[0x02],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0x2B,
+        params: &[0x73],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x0A],
+        delay_after: None,
+    }, // SET APGE9
+    LcdCommand {
+        addr: 0x29,
+        params: &[0x10],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0xFE,
+        params: &[0x00],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0x51,
+        params: &[0xff],
+        delay_after: None,
+    }, // Set brightness (175 in decimal)
+    LcdCommand {
+        addr: 0x53,
+        params: &[0x20],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0x35,
+        params: &[0x00],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0x3A,
+        params: &[0x75],
+        delay_after: None,
+    }, // Interface Pixel Format 16bit/pixel
+    LcdCommand {
+        addr: 0xC4,
+        params: &[0x80],
+        delay_after: None,
+    },
+    LcdCommand {
+        addr: 0x11,
+        params: &[0x00],
+        delay_after: Some(120),
+    }, // Sleep Out with 120ms delay
+    LcdCommand {
+        addr: 0x29,
+        params: &[0x00],
+        delay_after: Some(120),
+    }, // Display ON with 120ms delay
+];
 
 /// RM67162 display in Rgb565 color mode.
 pub struct RM67162;
@@ -30,30 +153,25 @@ impl Model for RM67162 {
     {
         let madctl = SetAddressMode::from(options);
 
-        /*  match rst {
+        match rst {
             Some(ref mut rst) => self.hard_reset(rst, delay)?,
             None => dcs.write_command(SoftReset)?,
         }
-        delay.delay_us(150_000);
 
-        dcs.write_command(ExitSleepMode)?;
-        delay.delay_us(10_000);
+        delay.delay_us(200_000);
 
-        // set hw scroll area based on framebuffer size
-        dcs.write_command(madctl)?;
+        // Send initialization commands
+        for cmd in AMOLED_INIT_CMDS {
+            // Write the command address
+            dcs.write_raw(cmd.addr, cmd.params).unwrap();
 
-        dcs.write_command(SetInvertMode::new(options.invert_colors))?;
+            // Apply delay if specified
+            if let Some(ms) = cmd.delay_after {
+                delay.delay_ms(ms);
+            }
+        }
 
-        let pf = PixelFormat::with_all(BitsPerPixel::from_rgb_color::<Self::ColorFormat>());
-        dcs.write_command(SetPixelFormat::new(pf))?;
-        delay.delay_us(10_000);
-        dcs.write_command(EnterNormalMode)?;
-        delay.delay_us(10_000);
-        dcs.write_command(SetDisplayOn)?;
-
-        // DISPON requires some time otherwise we risk SPI data issues
-        delay.delay_us(120_000); */
-
+        info!("Display initialized");
         Ok(madctl)
     }
 
@@ -62,12 +180,12 @@ impl Model for RM67162 {
         DI: WriteOnlyDataCommand,
         I: IntoIterator<Item = Self::ColorFormat>,
     {
-        /* dcs.write_command(WriteMemoryStart)?;
+        dcs.write_command(WriteMemoryStart)?;
 
         let mut iter = colors.into_iter().map(Rgb565::into_storage);
 
         let buf = DataFormat::U16BEIter(&mut iter);
-        dcs.di.send_data(buf)?; */
+        dcs.di.send_data(buf)?;
         Ok(())
     }
 }
