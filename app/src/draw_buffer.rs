91bc9277d45b39e34092ab9f5ae35f11bd88d3c7
Eric Funk <psytraxx@users.noreply.github.com>
2025-01-11 13:25:57 +0100

    Refactor (#7)
    * feat: add DrawBuffer struct for managing display line buffers and rendering
    
    * feat: add launch configuration for ESP32-S3 debugging in VSCode
    
    * feat: implement DrawBuffer initialization with display configuration

---
+++ app/src/draw_buffer.rs
@@ -0,0 +1,78 @@
+use embedded_graphics_core::pixelcolor::raw::RawU16;
+use embedded_hal::digital::OutputPin;
+use esp_hal::delay::Delay;
+use mipidsi::interface::{Interface, InterfacePixelFormat};
+use mipidsi::models::Model;
+use mipidsi::options::{Orientation, Rotation};
+use mipidsi::{Builder, Display as MipiDisplay};
+use s3_display_amoled_touch_drivers::rm67162::RM67162;
+use slint::platform::software_renderer::{LineBufferProvider, Rgb565Pixel};
+
+pub struct DrawBuffer<'a, DI, MODEL, RST>
+where
+    DI: Interface<Word = u8>,
+    MODEL: Model,
+    MODEL::ColorFormat: InterfacePixelFormat<DI::Word>,
+    RST: OutputPin,
+{
+    pub display: MipiDisplay<DI, MODEL, RST>,
+    pub line_buffer: &'a mut [Rgb565Pixel],
+}
+
+impl<DI, RST> DrawBuffer<'_, DI, RM67162, RST>
+where
+    DI: Interface<Word = u8>,
+    RST: OutputPin,
+{
+    pub fn new<'a>(
+        di: DI,
+        line_buffer: &'a mut [Rgb565Pixel],
+        rst: RST,
+        delay: &mut Delay,
+    ) -> DrawBuffer<'a, DI, RM67162, RST> {
+        // Initialize display
+        let display = Builder::new(RM67162, di)
+            .orientation(Orientation {
+                mirrored: false,
+                rotation: Rotation::Deg270,
+            })
+            .reset_pin(rst)
+            .init(delay)
+            .unwrap();
+
+        DrawBuffer {
+            display,
+            line_buffer,
+        }
+    }
+}
+
+impl<DI, RST> LineBufferProvider for &mut DrawBuffer<'_, DI, RM67162, RST>
+where
+    DI: Interface<Word = u8>,
+    RST: OutputPin,
+{
+    type TargetPixel = Rgb565Pixel;
+
+    fn process_line(
+        &mut self,
+        line: usize,
+        range: core::ops::Range<usize>,
+        render_fn: impl FnOnce(&mut [Self::TargetPixel]),
+    ) {
+        let buffer = &mut self.line_buffer[range.clone()];
+
+        render_fn(buffer);
+
+        // We send empty data just to get the device in the right window
+        self.display
+            .set_pixels(
+                range.start as u16,
+                line as _,
+                range.end as u16,
+                line as u16,
+                buffer.iter().map(|x| RawU16::new(x.0).into()),
+            )
+            .expect("set_pixels failed");
+    }
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -8,26 +8,31 @@ use alloc::string::ToString;
 use core::cell::RefCell;
 use critical_section::Mutex;
 use defmt::{error, info};
-use display::{Display, DisplayPeripherals};
+use draw_buffer::DrawBuffer;
 use embedded_hal::i2c::I2c as I2CBus;
 use embedded_hal_bus::i2c::CriticalSectionDevice;
+use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_alloc::psram_allocator;
 use esp_hal::delay::Delay;
-use esp_hal::gpio::{Input, Pull};
+use esp_hal::dma::{Dma, DmaRxBuf, DmaTxBuf};
+use esp_hal::gpio::{Input, Level, Output, Pull};
 use esp_hal::i2c::master::I2c;
-use esp_hal::prelude::*;
+use esp_hal::spi::master::{Config, Spi, SpiDmaBus};
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::xtensa_lx::singleton;
-use s3_display_amoled_touch_drivers::bq25896::BQ25896;
-use s3_display_amoled_touch_drivers::cst816s::CST816S;
-use slint::platform::software_renderer::{MinimalSoftwareWindow, RepaintBufferType};
+use esp_hal::{dma_buffers, prelude::*};
+use mipidsi::interface::SpiInterface;
+use s3_display_amoled_touch_drivers::{bq25896::BQ25896, cst816s::CST816S};
+use slint::platform::software_renderer::{MinimalSoftwareWindow, RepaintBufferType, Rgb565Pixel};
 use slint::platform::{Platform, PointerEventButton};
 use slint::{LogicalPosition, PhysicalSize};
 use {defmt_rtt as _, esp_backtrace as _};
 
+slint::include_modules!();
+
 extern crate alloc;
 
-mod display;
+mod draw_buffer;
 
 pub const DISPLAY_HEIGHT: u16 = 240;
 
@@ -37,21 +42,29 @@ pub const DISPLAY_WIDTH: u16 = 536;
 /// I2C address of BQ25896 PMU
 const BQ25896_SLAVE_ADDRESS: u8 = 0x6B;
 
+// credits to
+// https://github.com/slint-ui/slint/blob/master/examples/mcu-board-support/esp32_s3_box.rs and
+// https://github.com/Yandrik/kolibri-cyd-evaluation-apps/blob/b089aef31cc64e294bd64b2032356fb70789ad65/app/src/bin/exapp-slint-timer.rs#L56
+// for the initial code
+
 #[entry]
 fn main() -> ! {
     esp_alloc::heap_allocator!(72 * 1024);
 
-    let delay = Delay::new();
+    // Initialize delay
+    let mut delay = Delay::new();
 
+    // Initialize peripherals
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::Clock240MHz;
         config
     });
 
+    // Initialize PSRAM allocator
     psram_allocator!(peripherals.PSRAM, esp_hal::psram);
 
-    // initalize i2c bus
+    // Initialize I2C bus
     let i2c = I2c::new(peripherals.I2C0, esp_hal::i2c::master::Config::default())
         .with_sda(peripherals.GPIO3)
         .with_scl(peripherals.GPIO2)
@@ -61,7 +74,7 @@ fn main() -> ! {
         singleton!(:Mutex<RefCell<I2c<'_, esp_hal::Async>>> = Mutex::new(RefCell::new(i2c)))
             .expect("Failed to create I2C mutex");
 
-    // initalize bq25896 charger
+    // Initialize BQ25896 charger
     let mut pmu = BQ25896::new(
         CriticalSectionDevice::new(i2c_ref_cell),
         BQ25896_SLAVE_ADDRESS,
@@ -69,45 +82,80 @@ fn main() -> ! {
     .expect("BQ25896 init failed");
 
     pmu.set_adc_enabled().expect("set_adc_enabled failed");
-
     pmu.set_fast_charge_current_limit(2048)
         .expect("set_fast_charge_current_limit failed");
 
     info!("PMU chip id: {}", pmu.get_chip_id().unwrap());
 
-    // initalize touchpad
+    // Initialize touchpad
     let touch_int = peripherals.GPIO21;
     let touch_int = Input::new(touch_int, Pull::None);
-
     let mut touchpad = CST816S::new(CriticalSectionDevice::new(i2c_ref_cell), touch_int, delay);
 
+    // Detect SPI model
     detect_spi_model(CriticalSectionDevice::new(i2c_ref_cell));
 
-    let display_peripherals = DisplayPeripherals {
-        sck: peripherals.GPIO47,
-        mosi: peripherals.GPIO18,
-        cs: peripherals.GPIO6,
-        dc: peripherals.GPIO7,
-        rst: peripherals.GPIO17,
-        pmicen: peripherals.GPIO38,
-        spi: peripherals.SPI2,
-        dma: peripherals.DMA,
-    };
+    // Initialize PMICEN pin
+    let mut pmicen = Output::new_typed(peripherals.GPIO38, Level::Low);
+    pmicen.set_high();
+    info!("PMICEN set high");
 
+    // Initialize display SPI peripherals
+    let dma = Dma::new(peripherals.DMA);
+    let sck = Output::new(peripherals.GPIO47, Level::Low);
+    let mosi = Output::new(peripherals.GPIO18, Level::Low);
+    let cs = Output::new(peripherals.GPIO6, Level::High);
+
+    // Configure SPI
+    let spi = Spi::new_with_config(
+        peripherals.SPI2,
+        Config {
+            frequency: 75.MHz(),
+            ..Config::default()
+        },
+    )
+    .with_sck(sck)
+    .with_mosi(mosi)
+    .with_dma(
+        dma.channel0
+            .configure(false, esp_hal::dma::DmaPriority::Priority0),
+    );
+
+    // Configure SPI DMA buffers
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
+
+    let spi = SpiDmaBus::new(spi, dma_rx_buf, dma_tx_buf);
+
+    // Initialize SPI device
+    let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
     let mut buffer = [0_u8; 512];
-    let mut display = Display::new(display_peripherals, &mut buffer).expect("Display init failed");
+    let di = SpiInterface::new(
+        spi_device,
+        Output::new(peripherals.GPIO7, Level::Low),
+        &mut buffer,
+    );
+
+    let rst = Output::new(peripherals.GPIO17, Level::High);
+
+    // Initialize buffer provider
+    let line_buffer = &mut [Rgb565Pixel(0); DISPLAY_WIDTH as usize];
+    let mut buffer_provider = DrawBuffer::new(di, line_buffer, rst, &mut delay);
 
     let window = MinimalSoftwareWindow::new(RepaintBufferType::ReusedBuffer);
     let size = PhysicalSize::new(DISPLAY_WIDTH.into(), DISPLAY_HEIGHT.into());
     window.set_size(size);
-    let backend = Backend {
+
+    // Set the platform for Slint
+    let backend = Box::new(Backend {
         window: window.clone(),
-    };
+    });
+    slint::platform::set_platform(backend).expect("set_platform failed");
 
-    slint::platform::set_platform(Box::new(backend)).unwrap();
-
+    // Initialize UI
     let ui = AppWindow::new().expect("UI init failed");
-
+    // Set up UI update callback
     ui.on_request_update({
         let ui_handle = ui.as_weak();
         move || {
@@ -122,44 +170,41 @@ fn main() -> ! {
     let mut touch_registered = false;
 
     loop {
+        // Update timers and animations
         slint::platform::update_timers_and_animations();
 
-        if !window.has_active_animations() {
-            // if no animation is running, wait for the next input event
-            if let Some(touch_event) = touchpad.read_touch(true).expect("read touch failed") {
-                let position = LogicalPosition::new(
-                    DISPLAY_WIDTH as f32 - touch_event.x as f32,
-                    DISPLAY_HEIGHT as f32 - touch_event.y as f32,
-                );
+        // Read touch events
+        if let Some(touch_event) = touchpad.read_touch(true).expect("read touch failed") {
+            let position = LogicalPosition::new(
+                DISPLAY_WIDTH as f32 - touch_event.x as f32,
+                DISPLAY_HEIGHT as f32 - touch_event.y as f32,
+            );
 
-                //info!("Touch event: {:?}", defmt::Debug2Format(&touch_event));
-
-                if touch_event.points > 0 && !touch_registered {
-                    //info!("Touch pressed");
-                    window.dispatch_event(slint::platform::WindowEvent::PointerPressed {
-                        position,
-                        button: PointerEventButton::Left,
-                    });
-                    touch_registered = true;
-                } else if touch_event.points > 0 && touch_registered {
-                    //info!("Touch down");
-                    window.dispatch_event(slint::platform::WindowEvent::PointerMoved { position });
-                } else if touch_event.points == 0 && touch_registered {
-                    //info!("Touch released");
-                    window.dispatch_event(slint::platform::WindowEvent::PointerReleased {
-                        position,
-                        button: PointerEventButton::Left,
-                    });
-                    touch_registered = false;
-                }
+            // Handle touch events
+            if touch_event.points > 0 && !touch_registered {
+                window.dispatch_event(slint::platform::WindowEvent::PointerPressed {
+                    position,
+                    button: PointerEventButton::Left,
+                });
+                touch_registered = true;
+            } else if touch_event.points > 0 && touch_registered {
+                window.dispatch_event(slint::platform::WindowEvent::PointerMoved { position });
+            } else if touch_event.points == 0 && touch_registered {
+                window.dispatch_event(slint::platform::WindowEvent::PointerReleased {
+                    position,
+                    button: PointerEventButton::Left,
+                });
+                touch_registered = false;
             }
         }
 
-        // Draw the scene if something needs to be drawn.
+        // Draw the scene if something needs to be drawn
         window.draw_if_needed(|renderer| {
-            //info!("Drawing scene");
-            renderer.render_by_line(&mut display);
+            renderer.render_by_line(&mut buffer_provider);
         });
+        if window.has_active_animations() {
+            continue;
+        }
     }
 }
 
@@ -182,8 +227,6 @@ where
     }
 }
 
-slint::include_modules!();
-
 struct Backend {
     window: Rc<MinimalSoftwareWindow>,
 }
@@ -198,13 +241,14 @@ impl Platform for Backend {
     }
 
     fn duration_since_start(&self) -> core::time::Duration {
+        // Calculate duration since start
         core::time::Duration::from_millis(
             SystemTimer::now() * 1_000 / SystemTimer::ticks_per_second(),
         )
     }
 
-    // fn run_event_loop(&self) -> Result<(), slint::PlatformError>
     fn debug_log(&self, arguments: core::fmt::Arguments) {
+        // Log debug messages
         info!("Slint: {}", arguments.to_string().as_str());
     }
 }
